db793cf6173a52a99c1a87b85eb69923fe7e3103
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-17 15:37:55 +0200

    asdfasdf

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-include 'header.php';
+include 'header.php'; 
 include 'database.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -7,17 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $gebruikersnaam = $_POST['gebruikersnaam'];
     $emailadres = $_POST['emailadres'];
     $wachtwoord = $_POST['wachtwoord'];
-    $wachtwoord_herhalen = $_POST['wachtwoord_herhalen'];
-
 
     $sql = "INSERT INTO gebruikers (gebruikersnaam, emailadres, wachtwoord, rol_id) 
             VALUES ('$gebruikersnaam', '$emailadres', '$wachtwoord', (SELECT rol_id FROM rol WHERE rol_naam = '$role'))";
 
     if ($conn->query($sql) === TRUE) {
-        echo "<div class='alert alert-success' role='alert'>Registratie gelukt!</div>"; // Success message
+        echo "<div class='alert alert-success' role='alert'>Registratie gelukt!</div>";
         echo "<script>setTimeout(function() { window.location.href = 'index.php'; }, 3000);</script>";
     } else {
-        echo "<div class='alert alert-danger' role='alert'>Error: " . $sql . "<br>" . $conn->error . "</div>"; // Error message
+        echo "<div class='alert alert-danger' role='alert'>Error: " . $sql . "<br>" . $conn->error . "</div>";
     }
 
     $conn->close();
