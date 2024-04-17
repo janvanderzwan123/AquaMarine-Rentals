29103e40898ee931a2b23d3cb38c8ea71c39ad09
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-17 15:51:58 +0200

    fafsdsasf

--- register.php
+++ register.php
@@ -1,28 +1,25 @@
 <?php
 include 'header.php'; 
 include 'database.php';
-echo 'begin registrer.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo 'begin if statement.php';
-    $role = $_POST['rol_id'];
+    $role = $_POST['role'];
     $gebruikersnaam = $_POST['gebruikersnaam'];
     $emailadres = $_POST['emailadres'];
     $wachtwoord = $_POST['wachtwoord'];
-    echo 'voor sql statement';
+
     $sql = "INSERT INTO gebruikers (gebruikersnaam, emailadres, wachtwoord, rol_id) 
             VALUES ('$gebruikersnaam', '$emailadres', '$wachtwoord', (SELECT rol_id FROM rol WHERE rol_naam = '$role'))";
-    echo 'na sql statement';
-    echo $conn->query($sql);
+
     if ($conn->query($sql) === TRUE) {
         echo "<div class='alert alert-success' role='alert'>Registratie gelukt!</div>";
         echo "<script>setTimeout(function() { window.location.href = 'index.php'; }, 3000);</script>";
-        echo 'iets';
     } else {
-        echo 'iets';
         echo "<div class='alert alert-danger' role='alert'>Error: " . $sql . "<br>" . $conn->error . "</div>";
     }
 
     $conn->close();
 }
+
 include 'footer.php';
 ?>
