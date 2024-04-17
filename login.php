7fdc22ecb3edd4f977f9eb1fbb8495829931c833
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-17 13:44:20 +0200

    hgfds

--- login.php
+++ login.php
@@ -6,14 +6,6 @@ session_start();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
 <body>
     <div class="container mt-5">
         <div class="row justify-content-center">
