cd91c5c9b92805fe412a64b37cb0fbb29ffc7733
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-17 11:55:17 +0200

    lkjhvg

--- header.php
+++ header.php
@@ -1,4 +1,10 @@
-<?php include 'database.php'; ?>
+<?php 
+include 'database.php'; 
+
+
+$loggedInUser = false;
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,16 +13,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AquaMarine Rentals</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> <!-- Font Awesome -->
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"> <!-- Pacifico font -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> 
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     <link rel="icon" type="image/png" href="favicon.png">
     <style>
-        /* Add shadow under header */
+
         header {
             box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1);
         }
-        /* Apply Pacifico font to headings */
         body {
             font-family: 'Pacifico', cursive;
         }
@@ -31,7 +35,11 @@
             </div>
             <h1 class="m-0">Aqua Marine Rentals</h1>
             <!-- Profile icon with link -->
-            <a href="profile.php" class="text-dark"><i class="far fa-user"></i></a> <!-- Font Awesome profile icon -->
+            <?php if ($loggedInUser): ?>
+                <a href="profile.php" class="text-dark"><i class="far fa-user"></i></a> <!-- Font Awesome profile icon -->
+            <?php else: ?>
+                <a href="login.php" class="text-dark"><i class="far fa-user"></i></a> <!-- Redirect to login.php if not logged in -->
+            <?php endif; ?>
         </div>
     </header>
 </body>
