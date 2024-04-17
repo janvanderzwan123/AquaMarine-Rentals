4ac4e731a8a1a15ebad6c0c9349ffdf0f51be31d
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-18 01:43:00 +0200

    F

--- index.php
+++ index.php
@@ -17,52 +17,54 @@ include 'header.php';
         </div>
         <div class="row">
             <!-- Filter Menu -->
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h3>Filteren</h3>
-                        <hr>
-                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
-                            <div class="form-group">
-                                <label for="datum">Datum</label>
-                                <input type="date" class="form-control" id="datum" name="datum">
-                            </div>
-                            <div class="form-group">
-                                <label for="locatie">Locatie</label>
-                                <input type="text" class="form-control" id="locatie" name="locatie">
-                            </div>
-                            <div class="form-group">
-                                <label for="type-boot">Soort boot</label>
-                                <select class="form-control" id="type-boot" name="type-boot">
-                                    <option value="">Kiezen</option>
-                                    <option value="sailing">Zeilboot</option>
-                                    <option value="motor">Motorboot</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="vermogen">Vermogen</label>
-                                <input type="number" class="form-control" id="vermogen" name="vermogen">
-                            </div>
-                            <div class="form-group">
-                                <label for="lengte">Lengte</label>
-                                <input type="number" class="form-control" id="lengte" name="lengte">
-                            </div>
-                            <div class="form-group">
-                                <label for="snelheid">Snelheid</label>
-                                <input type="number" class="form-control" id="snelheid" name="snelheid">
-                            </div>
-                            <div class="form-group">
-                                <label for="passagiers">Aantal passagiers</label>
-                                <input type="number" class="form-control" id="passagiers" name="passagiers">
-                            </div>
-                            <button type="submit" class="btn btn-primary" name="submit">Filters Toepassen</button>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-danger" name="reset">Filters Resetten</button>
-                            </div>
-                        </form>
-                    </div>
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-body">
+            <h3>Filteren</h3>
+            <hr>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+                <div class="form-group">
+                    <label for="datum">Datum</label>
+                    <input type="date" class="form-control" id="datum" name="datum" value="<?php echo isset($_GET['datum']) ? htmlspecialchars($_GET['datum']) : ''; ?>">
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="locatie">Locatie</label>
+                    <input type="text" class="form-control" id="locatie" name="locatie" value="<?php echo isset($_GET['locatie']) ? htmlspecialchars($_GET['locatie']) : ''; ?>">
+                </div>
+                <div class="form-group">
+                    <label for="type-boot">Soort boot</label>
+                    <select class="form-control" id="type-boot" name="type-boot">
+                        <option value="">Kiezen</option>
+                        <option value="sailing" <?php echo (isset($_GET['type-boot']) && $_GET['type-boot'] === 'sailing') ? 'selected' : ''; ?>>Zeilboot</option>
+                        <option value="motor" <?php echo (isset($_GET['type-boot']) && $_GET['type-boot'] === 'motor') ? 'selected' : ''; ?>>Motorboot</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="vermogen">Vermogen</label>
+                    <input type="number" class="form-control" id="vermogen" name="vermogen" value="<?php echo isset($_GET['vermogen']) ? htmlspecialchars($_GET['vermogen']) : ''; ?>">
+                </div>
+                <div class="form-group">
+                    <label for="lengte">Lengte</label>
+                    <input type="number" class="form-control" id="lengte" name="lengte" value="<?php echo isset($_GET['lengte']) ? htmlspecialchars($_GET['lengte']) : ''; ?>">
+                </div>
+                <div class="form-group">
+                    <label for="snelheid">Snelheid</label>
+                    <input type="number" class="form-control" id="snelheid" name="snelheid" value="<?php echo isset($_GET['snelheid']) ? htmlspecialchars($_GET['snelheid']) : ''; ?>">
+                </div>
+                <div class="form-group">
+                    <label for="passagiers">Aantal passagiers</label>
+                    <input type="number" class="form-control" id="passagiers" name="passagiers" value="<?php echo isset($_GET['passagiers']) ? htmlspecialchars($_GET['passagiers']) : ''; ?>">
+                </div>
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-primary" name="submit">Filters Toepassen</button>
+
+                <!-- Reset button -->
+                <a href="<?php echo $_SERVER['PHP_SELF']; ?>" class="btn btn-secondary">Filters Resetten</a>
+            </form>
+        </div>
+    </div>
+</div>
+
             <!-- Boat Listings -->
             <div class="col-md-9">
                 <?php include 'advertenties.php'; ?>
