1ba1aa44e4128488bc25d80598e59e550a3b5a11
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-17 15:42:05 +0200

    asdfasfdasdf

--- register.php
+++ register.php
@@ -1,16 +1,17 @@
 <?php
 include 'header.php'; 
 include 'database.php';
-
+echo 'begin registrer.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo 'begin if statement.php';
     $role = $_POST['role'];
     $gebruikersnaam = $_POST['gebruikersnaam'];
     $emailadres = $_POST['emailadres'];
     $wachtwoord = $_POST['wachtwoord'];
-
+    echo 'voor sql statement';
     $sql = "INSERT INTO gebruikers (gebruikersnaam, emailadres, wachtwoord, rol_id) 
             VALUES ('$gebruikersnaam', '$emailadres', '$wachtwoord', (SELECT rol_id FROM rol WHERE rol_naam = '$role'))";
-
+    echo 'na sql statement';
     if ($conn->query($sql) === TRUE) {
         echo "<div class='alert alert-success' role='alert'>Registratie gelukt!</div>";
         echo "<script>setTimeout(function() { window.location.href = 'index.php'; }, 3000);</script>";
