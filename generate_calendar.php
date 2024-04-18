85fa54144c832473622a08bc03c3cce4ce3bd3cf
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-18 03:59:23 +0200

    d

--- generate_calendar.php
+++ generate_calendar.php
@@ -30,15 +30,19 @@ for ($i = 1; $i <= $numDays; $i++) {
         }
     }
 
-    // Add a date box for the current day
-    $html .= '<label><input type="checkbox" name="selected_dates[]" value="' . $i . '">' . $i . '</label>';
+    if ($eventTitles[$i] === 'Beschikbaar') {
+        $html .= '<label style="background-color: green;"><input type="checkbox" name="selected_dates[]" value="' . $i . '" checked>' . $i . '</label>';
+    } else {
+        $html .= '<label style="background-color: red;"><input type="checkbox" name="selected_dates[]" value="' . $i . '">' . $i . '</label>';
+    }
+
 
     // Close the row at the end of the week or at the end of the month
     if ($i == $numDays || ($i + $firstDayOfWeek - 1) % 7 == 0) {
         $html .= '</div>';
     }
 }
-
+echo $eventTitle[$i];
 // Close the calendar form
 $html .= '</div><button type="submit" class="btn btn-primary save-button">Kalender opslaan</button></form>';
 
