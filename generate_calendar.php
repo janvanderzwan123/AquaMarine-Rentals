51e065da3eeee05e1b91796721ca8d634eb44768
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-18 03:46:13 +0200

    d

--- generate_calendar.php
+++ generate_calendar.php
@@ -1,14 +1,4 @@
 <?php
-// Get current month and year
-$month = date('n');
-$year = date('Y');
-
-// Get number of days in the month
-$numDays = date('t', mktime(0, 0, 0, $month, 1, $year));
-
-// Get the first day of the week (1 = Monday, 7 = Sunday)
-$firstDayOfWeek = date('N', mktime(0, 0, 0, $month, 1, $year));
-
 // Start generating HTML
 $html = '<form action="save_calendar.php" method="post"><div class="calendar">';
 
@@ -21,12 +11,21 @@ for ($i = 1; $i <= $numDays; $i++) {
     // Add empty date boxes for days before the first day of the month
     if ($i == 1) {
         for ($j = 1; $j < $firstDayOfWeek; $j++) {
-            $html .= '<div class="date"></div>';
+            // Add the day-box with appropriate ID based on event_title
+            if ($eventTitle[$i] === 'Beschikbaar') {
+                $html .= '<div id="day-box" class="date"></div>';
+            } else {
+                $html .= '<div id="selected" class="date"></div>';
+            }
         }
     }
 
     // Add a date box for the current day
-    $html .= '<label><input type="checkbox" name="selected_dates[]" value="' . $i . '">' . $i . '</label>';
+    if ($eventTitle[$i] === 'Beschikbaar') {
+        $html .= '<label><input type="checkbox" id="day-box" name="selected_dates[]" value="' . $i . '">' . $i . '</label>';
+    } else {
+        $html .= '<label><input type="checkbox" id="selected" name="selected_dates[]" value="' . $i . '">' . $i . '</label>';
+    }
 
     // Close the row at the end of the week or at the end of the month
     if ($i == $numDays || ($i + $firstDayOfWeek - 1) % 7 == 0) {
