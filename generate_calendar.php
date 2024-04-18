58a2e10a7e5ab5831d66643c53fe46e95789c22f
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-18 03:07:58 +0200

    d

--- generate_calendar.php
+++ generate_calendar.php
@@ -15,11 +15,11 @@ for ($i = 1; $i <= $numDays; $i++) {
 
     if ($i == 1) {
         for ($j = 1; $j < $firstDayOfWeek; $j++) {
-            $html .= '<div class="date"></div>';
+            $html .= '<div class="date" style="width: calc(100% / 7);"></div>';
         }
     }
 
-    $html .= '<div style="border: 1px solid black;" class="date">' . $i . '</div>';
+    $html .= '<div style="border: 1px solid black; width: calc(100% / 7);" class="date">' . $i . '</div>';
 
     if ($i == $numDays || ($i + $firstDayOfWeek - 1) % 7 == 0) {
         $html .= '</div>';
