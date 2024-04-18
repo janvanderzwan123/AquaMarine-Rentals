1195dd3596211c45073496bf3d24d5545a6fc7ba
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-18 02:34:18 +0200

    c

--- new_listing.php
+++ new_listing.php
@@ -12,9 +12,16 @@
                             <input type="text" class="form-control" id="boot_naam" name="boot_naam" required>
                         </div>
                         <div class="form-group">
-                            <label for="boot_type">Boot Type:</label>
-                            <input type="text" class="form-control" id="boot_type" name="boot_type" required>
-                        </div>
+                                <label for="boot_type">Soort boot</label>
+                                <select class="form-control" id="boot_type" name="boot_type" required>
+                                    <option value="">Selecteer type...</option>
+                                    <option value="Kano" <?php echo (isset($_POST['boot_type']) && $_POST['boot_type'] === 'Kano') ? 'selected' : ''; ?>>Kano</option>
+                                    <option value="Motorboot" <?php echo (isset($_POST['boot_type']) && $_POST['boot_type'] === 'Motorboot') ? 'selected' : ''; ?>>Motorboot</option>
+                                    <option value="Zeilboot" <?php echo (isset($_POST['boot_type']) && $_POST['boot_type'] === 'Zeilboot') ? 'selected' : ''; ?>>Zeilboot</option>
+                                    <option value="Zeilschip" <?php echo (isset($_POST['boot_type']) && $_POST['boot_type'] === 'Zeilschip') ? 'selected' : ''; ?>>Zeilschip</option>
+                                    <option value="Kajak" <?php echo (isset($_POST['boot_type']) && $_POST['boot_type'] === 'Kajak') ? 'selected' : ''; ?>>Kajak</option>
+                                </select>
+                            </div>
                         <div class="form-group">
                             <label for="locatie">Locatie:</label>
                             <input type="text" class="form-control" id="locatie" name="locatie" required>
@@ -22,6 +29,10 @@
                         <div class="form-group">
                             <label for="vermogen">Vermogen:</label>
                             <input type="number" class="form-control" id="vermogen" name="vermogen" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="lengte">Lengte (meters)</label>
+                            <input type="number" class="form-control" id="lengte" name="lengte" placeholder="Enter length in meters" required>
                         </div>
                         <div class="form-group">
                             <label for="prijs_per_dag">Prijs per dag (€):</label>
