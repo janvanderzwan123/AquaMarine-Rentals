de73beb5574366f190b829a37e93fe48691db761
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-22 09:55:31 +0200

    adssf

--- profile.php
+++ profile.php
@@ -30,8 +30,8 @@ if ($result->num_rows > 0) {
 
 <div class="container mt-5">
     <center><h1>Welkom, <?php echo $username; ?>!</h1><br></center>
-    <a href="new_listing.php" class="btn btn-primary">Nieuwe advertentie</a><br>
     <h3>Jouw advertenties:</h3><br>
+    <a href="new_listing.php" class="btn btn-primary">Nieuwe advertentie</a><br>
     <div class="row">
         <?php if (!empty($advertisements)): ?>
             <?php foreach ($advertisements as $advertisement): ?>
