330a7e67260a8ce753160d12a4c74c93de9e945f
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-22 09:23:52 +0200

    ASD

--- display_calendar.php
+++ display_calendar.php
@@ -9,8 +9,11 @@ if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
 
 $username = $_SESSION['username'];
 
-$sql = "SELECT gebruiker_id FROM gebruikers WHERE gebruikersnaam = '$username'";
-$result = $conn->query($sql);
+// Secure the initial user query
+$stmt = $conn->prepare("SELECT gebruiker_id FROM gebruikers WHERE gebruikersnaam = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
@@ -22,48 +25,34 @@ if ($result->num_rows > 0) {
 $month = date('n');
 $year = date('Y');
 $numDays = date('t', mktime(0, 0, 0, $month, 1, $year));
-
 $firstDayOfWeek = date('N', mktime(0, 0, 0, $month, 1, $year));
-$eventTitles = [];
 
-for ($i = 1; $i <= $numDays; $i++) {
-    $start_date = date('Y-m-d', mktime(0, 0, 0, $month, $i, $year));
-    $end_date = date('Y-m-d H:i:s', strtotime($start_date . ' + 24 hours'));
+// Pre-fill event titles with 'Onbeschikbaar'
+$eventTitles = array_fill(1, $numDays, 'Onbeschikbaar');
 
-    $sql = "SELECT event_title FROM verhuurder_calendar WHERE start_date = ? AND end_date = ? AND gebruiker_id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssi", $start_date, $end_date, $gebruiker_id);
-    $stmt->execute();
-    $result = $stmt->get_result();
+$sql = "SELECT DAY(start_date) AS day, event_title FROM verhuurder_calendar WHERE MONTH(start_date) = ? AND YEAR(start_date) = ? AND gebruiker_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("iii", $month, $year, $gebruiker_id);
+$stmt->execute();
+$result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $eventTitles[$i] = $row['event_title'];
-    } else {
-        $eventTitles[$i] = 'Onbeschikbaar';
-    }
+while ($row = $result->fetch_assoc()) {
+    $eventTitles[$row['day']] = $row['event_title'];
 }
 
 $html = '<div class="calendar">';
-for ($i = 1; $i <= $numDays; $i++) {
-    if ($i == 1 || ($i - 1) % 7 == 0) {
+for ($i = 1; $i <= $numDays + $firstDayOfWeek - 1; $i++) {
+    if (($i - 1) % 7 == 0) {
         $html .= '<div class="row">';
     }
-    if ($i == 1) {
-        for ($j = 1; $j < $firstDayOfWeek; $j++) {
-            if ($eventTitles[$i] === 'Beschikbaar') {
-                $html .= '<div id="day-box" class="date"></div>';
-            } else {
-                $html .= '<div id="selected" class="date"></div>';
-            }
-        }
-    }
-    if ($eventTitles[$i] === 'Beschikbaar') {
-        $html .= '<div style="background-color: green;">' . $i . '</div>';
+    if ($i < $firstDayOfWeek) {
+        $html .= '<div class="date"></div>';
     } else {
-        $html .= '<div style="background-color: red;">' . $i . '</div>';
+        $day = $i - $firstDayOfWeek + 1;
+        $class = $eventTitles[$day] === 'Beschikbaar' ? 'day-box' : 'selected';
+        $html .= '<div class="' . $class . '">' . $day . '</div>';
     }
-    if ($i == $numDays || ($i + $firstDayOfWeek - 1) % 7 == 0) {
+    if ($i % 7 == 0 || $i == $numDays + $firstDayOfWeek - 1) {
         $html .= '</div>';
     }
 }
