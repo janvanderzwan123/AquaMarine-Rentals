1947c5bfec19c2f3cd00e4f989dee8a3eb2c7532
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-22 13:27:55 +0200

    asdf

--- save_calendar.php
+++ save_calendar.php
@@ -36,41 +36,31 @@ function getNumDaysInMonth() {
 
 $advertentieID = $_POST['advertentie_id'];
 
-function getBoatCalendarEvents($conn, $advertentieID) {
-    $sql = "SELECT start_date, event_title FROM verhuurder_calendar WHERE advertentie_id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $advertentieID);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $events = [];
-    while ($row = $result->fetch_assoc()) {
-        $events[$row['start_date']] = $row['event_title'];
-    }
-    return $events;
-}
+function updateCalendarEvents($conn, $advertentieID) {
+    $numDays = getNumDaysInMonth();
 
-$calendarEvents = getBoatCalendarEvents($conn, $advertentieID);
-$numDays = getNumDaysInMonth();
-
-for ($i = 1; $i <= $numDays; $i++) {
-    $date = date('Y-m-d', mktime(0, 0, 0, date('n'), $i, date('Y')));
-    if (isset($_POST['selected_dates'])) {
-        if (in_array($i, $_POST['selected_dates'])) {
-            $eventTitle = 'Beschikbaar';
+    for ($i = 1; $i <= $numDays; $i++) {
+        $date = date('Y-m-d', mktime(0, 0, 0, date('n'), $i, date('Y')));
+        if (isset($_POST['selected_dates'])) {
+            if (in_array($i, $_POST['selected_dates'])) {
+                $eventTitle = 'Beschikbaar';
+            } else {
+                $eventTitle = 'Onbeschikbaar';
+            }
+            $sql = "UPDATE verhuurder_calendar SET event_title = ? WHERE advertentie_id = ? AND start_date = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sis", $eventTitle, $advertentieID, $date);
+            $stmt->execute();
         } else {
-            $eventTitle = 'Onbeschikbaar';
+            $sql = "UPDATE verhuurder_calendar SET event_title = 'Onbeschikbaar' WHERE advertentie_id = ? AND start_date = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("is", $advertentieID, $date);
+            $stmt->execute();
         }
-        $sql = "UPDATE verhuurder_calendar SET event_title = ? WHERE advertentie_id = ? AND start_date = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sis", $eventTitle, $advertentieID, $date);
-        $stmt->execute();
-    } else {
-        $sql = "UPDATE verhuurder_calendar SET event_title = 'Onbeschikbaar' WHERE advertentie_id = ? AND start_date = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("is", $advertentieID, $date);
-        $stmt->execute();
     }
 }
+
+updateCalendarEvents($conn, $advertentieID);
 
 header("Location: profile.php");
 exit();
