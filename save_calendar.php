fc237c8d3d07f56abe0cafe21abd835743b1b054
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-22 13:01:39 +0200

    ASDF

--- save_calendar.php
+++ save_calendar.php
@@ -39,14 +39,25 @@ function initializeCalendar($conn, $advertentieID) {
     $year = date('Y');
     $numDays = date('t', mktime(0, 0, 0, $month, 1, $year));
 
-    for ($i = 1; $i <= $numDays; $i++) {
-        $date = date('Y-m-d', mktime(0, 0, 0, $month, $i, $year));
-        $sql = "INSERT INTO verhuurder_calendar (start_date, end_date, event_title, advertentie_id) VALUES (?, ?, 'Onbeschikbaar', ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ssi", $date, $date, $advertentieID);
-        $stmt->execute();
+    $sql_check_calendar = "SELECT COUNT(*) AS num_rows FROM verhuurder_calendar WHERE advertentie_id = ?";
+    $stmt_check_calendar = $conn->prepare($sql_check_calendar);
+    $stmt_check_calendar->bind_param("i", $advertentieID);
+    $stmt_check_calendar->execute();
+    $result_check_calendar = $stmt_check_calendar->get_result();
+    $row_check_calendar = $result_check_calendar->fetch_assoc();
+    $num_rows_calendar = $row_check_calendar['num_rows'];
+
+    if ($num_rows_calendar == 0) {
+        for ($i = 1; $i <= $numDays; $i++) {
+            $date = date('Y-m-d', mktime(0, 0, 0, $month, $i, $year));
+            $sql = "INSERT INTO verhuurder_calendar (start_date, end_date, event_title, advertentie_id) VALUES (?, ?, 'Onbeschikbaar', ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("ssi", $date, $date, $advertentieID);
+            $stmt->execute();
+        }
     }
 }
+
 
 $advertentieID = $_POST['advertentie_id'];
 
