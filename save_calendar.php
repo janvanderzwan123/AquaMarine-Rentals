c932a2940f3fab4ac0a027f9793bb227403dbb85
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-23 10:43:53 +0200

    asdf

--- save_calendar.php
+++ save_calendar.php
@@ -50,7 +50,7 @@ function updateCalendarEvents($conn, $advertentieID) {
             $sql = "UPDATE verhuurder_calendar SET event_title = 'Beschikbaar' WHERE advertentie_id = $advertentieID AND start_date = $date";
             echo '<div>advertentie id = ' . $advertentieID . '</div>';
             echo '<div>datum = ' . $date . '</div>';
-            echo '<div>datum = ' . gettype($date) . '</div>';
+            echo '<div>datum = ' . getdate($date) . '</div>';
 
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("sis", $eventTitle, $advertentieID, $date);
