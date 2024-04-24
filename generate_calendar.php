addd6221ffccc972aec77ffa0e181cd7adca9923
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-24 10:58:03 +0200

    sdhf

--- generate_calendar.php
+++ generate_calendar.php
@@ -58,15 +58,18 @@ function initializeCalendar($conn, $advertentieID) {
     $year = date('Y');
     $numDays = date('t', mktime(0, 0, 0, $month, 1, $year));
 
-    $sql_check_calendar = "SELECT COUNT(*) AS num_rows FROM verhuurder_calendar WHERE advertentie_id = ?";
+
+    $startOfMonth = date('Y-m-01', mktime(0, 0, 0, $month, 1, $year));
+    $endOfMonth = date('Y-m-t', mktime(0, 0, 0, $month, 1, $year));
+
+    $sql_check_calendar = "SELECT COUNT(*) AS num_rows FROM verhuurder_calendar WHERE advertentie_id = ? AND start_date BETWEEN ? AND ?";
     $stmt_check_calendar = $conn->prepare($sql_check_calendar);
-    $stmt_check_calendar->bind_param("i", $advertentieID);
+    $stmt_check_calendar->bind_param("iss", $advertentieID, $startOfMonth, $endOfMonth);
     $stmt_check_calendar->execute();
     $result_check_calendar = $stmt_check_calendar->get_result();
     $row_check_calendar = $result_check_calendar->fetch_assoc();
-    $num_rows_calendar = $row_check_calendar['num_rows'];
 
-    if ($num_rows_calendar == 0) {
+    if ($row_check_calendar['num_rows'] == 0) {
         for ($i = 1; $i <= $numDays; $i++) {
             $date = date('Y-m-d', mktime(0, 0, 0, $month, $i, $year));
             $sql = "INSERT INTO verhuurder_calendar (start_date, end_date, event_title, advertentie_id) VALUES (?, ?, 'Onbeschikbaar', ?)";
@@ -77,35 +80,30 @@ function initializeCalendar($conn, $advertentieID) {
     }
 }
 
+
 function generateCalendar($numDays, $firstDayOfWeek, $eventTitles) {
     $advertentie_id = $_GET['advertentie_id'];
     $html = "<form action='save_calendar.php?advertentie_id=" . $advertentie_id . "' method='post'><div class='calendar'>";
 
 
     for ($i = 1; $i <= $numDays; $i++) {
-        if ($i == 1 || ($i - 1) % 7 == 0) {
+        if ($i == 1 || ($i + $firstDayOfWeek - 2) % 7 == 0) {
             $html .= '<div class="row">';
         }
-        if ($i == 1) {
-            for ($j = 1; $j < $firstDayOfWeek; $j++) {
-                if ($eventTitles[$i] === 'Beschikbaar') {
-                    $html .= '<div id="day-box" class="date"></div>';
-                } else {
-                    $html .= '<div id="selected" class="date"></div>';
-                }
-            }
-        }
-        if ($eventTitles[$i] === 'Beschikbaar') {
-            $html .= '<label style="background-color: green;"><input type="checkbox" name="selected_dates[]" value="' . $i . '" checked>' . $i . '</label>';
-        } else {
-            $html .= '<label style="background-color: red;"><input type="checkbox" name="selected_dates[]" value="' . $i . '">' . $i . '</label>';
-        }
+    
+        // Check if day is available
+        $backgroundColor = ($eventTitles[$i] === 'Beschikbaar') ? 'green' : 'red';
+        $checkedAttribute = ($eventTitles[$i] === 'Beschikbaar') ? 'checked' : '';
+    
+        $html .= "<label style='background-color: $backgroundColor;'><input type='checkbox' name='selected_dates[]' value='$i' $checkedAttribute>$i</label>";
+    
         if ($i == $numDays || ($i + $firstDayOfWeek - 1) % 7 == 0) {
-            $html .= '</div>';
+            $html .= '</div>'; // Close the row div
         }
     }
-
-    $html .= '</div><button type="submit" class="btn btn-primary save-button">Kalender opslaan</button></form>';
+    
+    $html .= "</div><button type='submit' class='btn btn-primary save-button'>Kalender opslaan</button></form>";
     return $html;
+    
 }
 ?>
