34d28529fce344e33fa17c46d0264ac3cc880aae
Jan van der Zwan <70353@hoornbeeck.nl>
2024-04-26 08:39:58 +0200

    dsfg

--- reserveren.php
+++ reserveren.php
@@ -3,21 +3,28 @@ include 'header.php';
 ?>
 
 <body>
-    <h2>Reserveren</h2>
-    <form action="process_reservation.php" method="POST">
-        <label for="selected_dates">Selecteer datums</label><br>
-        <!-- Calendar input field -->
-        <input type="date" id="selected_dates" name="selected_dates" multiple><br>
-
-        <label for="additional_info">Aanvullende informatie:</label><br>
-        <!-- Text area for additional information -->
-        <textarea id="additional_info" name="additional_info" rows="4" cols="50"></textarea><br>
-
-        <!-- Submit button -->
-        <button type="submit" name="submit">Reserveren</button>
-    </form>
-
-    <!-- Include necessary JavaScript files for calendar input -->
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Reserveren</div>
+                <div class="card-body">
+                    <form action="process_reservation.php" method="post">
+                        <div class="form-group">
+                            <label for="selected_dates">Selecteer Datum(s):</label>
+                            <input type="date" class="form-control" id="selected_dates" name="selected_dates" multiple>
+                        </div>
+                        <div class="form-group">
+                            <label for="additional_info">Aanvullende Informatie:</label>
+                            <textarea class="form-control" id="additional_info" name="additional_info" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Reserveren</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
 
